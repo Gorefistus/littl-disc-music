22371f999c0e9af35985c324102512858b1a978c
Gleb <stupakov@211.ru>
2018-10-20 00:16:54 +0700

    autoplay now picks a random song from related songs

--- common/streamHandler.js
+++ common/streamHandler.js
@@ -23,8 +23,10 @@ const songPlayer = {
                 .on('error', error => console.error(error));
         } else if (queueHandler.getAutoplay(guildId)) {
             const prevVideoObj = queueHandler.getLastSong(guildId);
-            const nextYTVideo = await youtube.searchVideos('', 1, { relatedToVideoId: prevVideoObj.id });
-            const nextYtVideoObj = await youtube.getVideoByID(nextYTVideo[0].id);
+            const maxSearchResults = 2;
+            const nextYTVideo = await youtube.searchVideos('', maxSearchResults, { relatedToVideoId: prevVideoObj.id });
+            const randomId = Math.floor(Math.random() * (nextYTVideo.length));
+            const nextYtVideoObj = await youtube.getVideoByID(nextYTVideo[randomId].id);
             const videoIndex = await queueHandler.addSongToQueue(guildId, nextYtVideoObj, queueHandler.getVoiceChannel(guildId));
             songPlayer.playYoutubeVideo({
                 guildId,
